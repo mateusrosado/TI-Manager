3d4849fe264c8ecc916042f8951dcae386306628
Pedro Silva <pedluc90@gmail.com>
2025-06-05 01:50:42 -0300

    Remover os ecos de teste comentados do LoginController

--- Controllers/LoginController.php
+++ Controllers/LoginController.php
@@ -1,17 +1,9 @@
 <?php
 // C:\xampp\htdocs\TI-Manager\Controllers\LoginController.php
 
-class LoginController extends Controller { // Confirme se você está estendendo a classe Controller
+class LoginController extends Controller {
     public function index() {
-        // Remove os 'echo' de teste.
-        // echo "<h1>Bem-vindo à Página de Login!</h1>";
-        // echo "<p>Agora precisamos carregar sua view de login da pasta Views/Login.</p>";
-
-        // Agora, vamos carregar a view de login.
-        // O caminho para Views/Login/index.php seria 'Login/index'.
-        $this->loadView('Login/index'); // Chame o método loadView da classe pai (Controller)
+        $this->loadView('Login/index');
     }
 
-    // Outros métodos de login, se houver
 }
-?>
