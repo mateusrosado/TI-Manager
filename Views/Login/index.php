d9ec0a6d3b4e4ae32d069434b1035589b3b9003e
Pedro Silva <pedluc90@gmail.com>
2025-06-05 01:51:18 -0300

    Refatorar a estrutura da página de login e atualizar o método do formulário

--- Views/Login/index.php
+++ Views/Login/index.php
@@ -1,23 +1,25 @@
 <!DOCTYPE html>
 <html lang="pt-BR">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login</title>
-    <link rel="stylesheet" href="<?= BASE_URL; ?>Assets/css/tema.css">
-    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=swap" rel="stylesheet">
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Login</title>
+  <link rel="stylesheet" href="<?= BASE_URL; ?>/assets/css/tema.css">
 </head>
 <body class="login-page">
-    <div class="container">
-
-        <div class="ball"></div>
-
-        <div class="form-container">
-            <h1>LOGIN</h1>
-            <form method="POST"> <input type="text" placeholder="Código de acesso" name="codigo_acesso"> <input type="password" placeholder="Senha" name="senha"> <a href="<?= BASE_URL; ?>SendEmail/">Esqueci minha senha</a>
-                <button type="submit">Login</button>
-            </form>
-        </div>
+  <div class="container">
+   
+    <div class="ball"></div>
+  
+    <div class="form-container">
+      <h1>LOGIN</h1>
+      <form>
+        <input type="text" placeholder="Código de acesso">
+        <input type="password" placeholder="Senha">
+          <a href="../SendEmail/">Esqueci minha senha</a>
+        <button type="button" onclick="window.location.href='../Home/'">Login</button>
+      </form>
     </div>
+  </div>
 </body>
 </html>
